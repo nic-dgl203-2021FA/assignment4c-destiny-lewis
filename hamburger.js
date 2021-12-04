1303fc235b8f56173003dded23c86fa94f674e2d
Destiny Lewis <70827545+destiny-lewis@users.noreply.github.com>
2021-12-04 15:54:09 -0800

    Style navigation menu for small screens
    - Reuse code from Assignment 4B to create responsive hamburger menu.
    
    - Apply bounce animation to navigation items.

---
+++ hamburger.js
@@ -0,0 +1,29 @@
+// Code reused from Assignment 4b
+
+const menuBtn = document.querySelector('.nav-btn');
+const hamburger = document.querySelector('.nav-btn__hamburger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.nav-menu');
+const navItems = document.querySelectorAll('.nav-menu__item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+  }
+}
